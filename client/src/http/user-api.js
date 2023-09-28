aa2030fe3f8741a9bf7ee28635d7f2a2afed8cd3
Sergentors705 <103508668+Sergentors705@users.noreply.github.com>
2023-09-28 23:00:06 +0600

    backend compiled with frontend

---
+++ client/src/http/user-api.js
@@ -0,0 +1,14 @@
+import {$authHost, $host} from "./index";
+
+export const registration = async (email, password) => {
+  const response = await $host.post('api/user/registration', {email, password, role: 'ADMIN'});
+  return response;
+}
+export const login = async (email, password) => {
+  const response = await $host.post('api/user/login', {email, password});
+  return response;
+}
+export const check = async (email, password) => {
+  const response = await $host.post('api/user/check');
+  return response;
+}
--- client/src/pages/auth/auth.jsx
+++ client/src/pages/auth/auth.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from "react-bootstrap/Container";
@@ -6,10 +6,22 @@ import Form from "react-bootstrap/Form";
 import Row from 'react-bootstrap/Row';
 import { useLocation } from "react-router-dom";
 import "./style.css";
+import { login, registration } from "../../http/user-api";
 
 function Auth() {
   const location = useLocation();
-  const isLogin = location.pathname === "/registration";
+  const isntLogin = location.pathname === "/registration";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const click = async () => {
+    if (isntLogin) {
+      const response = await registration(email, password);
+      console.log(response);
+    } else {
+      const response = await login();
+    }
+  }
 
   return (
     <Container
@@ -17,22 +29,33 @@ function Auth() {
       style={{height: window.innerHeight - 54}}
     >
       <Card className="p-5" style={{width: 600}}>
-        <h2 className="m-auto">{isLogin ? "Registration" : "Authorization"}</h2>
+        <h2 className="m-auto">{isntLogin ? "Registration" : "Authorization"}</h2>
         <Form className="d-flex flex-column p-1">
           <Form.Control
             className="mt-2"
             placeholder="Enter e-mail"
-          ></Form.Control>
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
           <Form.Control
+            type="password"
             className="mt-2"
             placeholder="Enter password"
-          ></Form.Control>
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
           <Row className="d-flex justify-content-between mt-4">
-            {isLogin
+            {isntLogin
             ?<Button href="/auth" style={{flexBasis: "fit-content"}} variant="warning">Authorization</Button>
             :<Button href="/registration" style={{flexBasis: "fit-content"}} variant="warning">Registration</Button>
             }
-            <Button style={{flexBasis: "fit-content"}} variant="success">{isLogin ? "Confirm" : "Log In"}</Button>
+            <Button
+              style={{flexBasis: "fit-content"}}
+              variant="success"
+              onClick={click}
+            >
+              {isntLogin ? "Register" : "Log In"}
+            </Button>
           </Row>
         </Form>
       </Card>
